38d3daf5ff66ea0017388ab1a62b7bdd28a40f5a
thach vu <vuhoangthach1402@gmail.com>
2023-05-22 00:21:42 +0700

    .

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const nodemailer = require('nodemailer');
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
-mongoose.connect('mongodb+srv://vuhoangthach1402:hoangthach123@cluster0.jdvz1fg.mongodb.net/hello', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://vuhoangthach1402:hoangthach123@cluster0.jdvz1fg.mongodb.net/hello',{});
 
 const userSchema = new mongoose.Schema({
     email: String,
