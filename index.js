a12bacc7a1663c30875f5b99013e60821a146bda
unknown <vuhoangthach1402@gmail.com>
2023-05-22 16:48:55 +0700

    .

--- index.js
+++ index.js
@@ -7,7 +7,22 @@ const nodemailer = require('nodemailer');
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
-mongoose.connect('mongodb+srv://hoangthach1402:hoangthach123@cluster0.jdvz1fg.mongodb.net',{useNewUrlParser: true, useUnifiedTopology: true});
+// const DATA_URL ="mongodb+srv://hoangthach1402:hoangthach123@cluster0.mmtet.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const connectDB = async () => {
+    try {
+      await mongoose.connect("mongodb+srv://hoangthach1402:hoangthach123@cluster0.mmtet.mongodb.net/loginSystem?retryWrites=true&w=majority", {
+        useUnifiedTopology: true,
+      });
+  
+      console.log("MongoDB connected");
+    } catch (error) {
+      console.log(error.message);
+      process.exit(1);
+    }
+  };
+  
+  connectDB();
+// mongoose.connect('mongodb+srv://hoangthach1402:hoangthach123@cluster0.jdvz1fg.mongodb.net',{useNewUrlParser: true, useUnifiedTopology: true});
 
 const userSchema = new mongoose.Schema({
     email: String,
@@ -18,10 +33,9 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model('User', userSchema);
 
 app.get('/', async (req,res)=>{
-    // const users = await User.find({}) ;
-    // res.json(users)
-    // res.send('hello login system') 
-    res.send('hello login system')
+    const users = await User.find();
+    res.json(users)
+    
 })
 app.post('/register', async (req, res) => {
     const { email, password } = req.body;
@@ -52,7 +66,7 @@ app.post('/register', async (req, res) => {
         from: 'vuhoangthach1402@gmail.com',
         to: user.email,
         subject: 'Verify Your Email',
-        text: `Hello, please verify your email by clicking the following link: http://localhost:3000/verify-email?token=${token}`
+        text: `Hello, please verify your email by clicking the following link: https://loginsystem.herokuapp.com/verify-email?token=${token}`
     };
 
     // Send email
@@ -69,21 +83,25 @@ app.post('/register', async (req, res) => {
 
 // ... Code for login goes here ...
 app.post('/login', async (req, res) => {
-    const {email, password} = req.body;
-    const user = await User.findOne({email});
-    
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+  
     if (user) {
+      if (user.isVerified) {
         const validPassword = await bcrypt.compare(password, user.password);
         if (validPassword) {
-            const token = jwt.sign({userId: user._id}, 'SECRET_KEY', {expiresIn: '1h'});
-            res.json({message: 'Login successful', token});
+          const token = jwt.sign({ userId: user._id }, 'SECRET_KEY', { expiresIn: '1h' });
+          res.json({ message: 'Đăng nhập thành công', token });
         } else {
-            res.send('Incorrect password');
+          res.send('Mật khẩu không chính xác');
         }
+      } else {
+        res.send('Người dùng chưa được xác minh');
+      }
     } else {
-        res.send('User not found');
+      res.send('Không tìm thấy người dùng');
     }
-});
+  });
 
 
 app.get('/verify-email', async (req, res) => {
@@ -104,7 +122,7 @@ app.get('/verify-email', async (req, res) => {
         await user.save();
         
         // Chuyển hướng người dùng tới trang đăng nhập hoặc trang chủ
-        res.redirect('/login');
+        res.send('xac thuc thanh cong, vui long dang nhap');
     } catch (err) {
         res.status(400).json({error: 'Lỗi xác thực.'});
     }
